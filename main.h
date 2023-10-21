e3c46ba41a6fb54d4a5dba4eefc147c3f65edd5e
eluemnaomi <eluemnaomi4@gmail.com>
2023-10-20 21:22:01 -0700

    messsage

--- main.h
+++ main.h
@@ -1,9 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * _printf - Custom printf function
+ * @format: Format string with optional conversion specifiers
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...);
 
-#endif 
+/**
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written. On error, returns -1.
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
--- print.c
+++ print.c
@@ -1,42 +1,65 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * _printf - Custom printf function.
- * @format: Format specifier.
- *
- * Return: Number of characters printed (excluding null byte).
+ * _printf - Custom printf function
+ * @format: Format string containing directives
+ * Return: Number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int count = 0; /* Count of characters printed */
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		*format == '%' ?
-			(format++, *format == 'c' ? (putchar(va_arg(args, int)), count++) :
-				*format == 's' ? (count += printf("%s", va_arg(args, char *))) :
-					(putchar('%'), putchar(*format), count += 2)) :
-			(putchar(*format), count++);
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++; /* Move past the '%' */
+            switch (*format)
+            {
+            case 'c':
+                /* Print character */
+                count += printf("%c", va_arg(args, int));
+                break;
+            case 's':
+                /* Print string */
+                count += printf("%s", va_arg(args, char *));
+                break;
+            case '%':
+                /* Print '%' */
+                count += printf("%%");
+                break;
+            default:
+                /* Invalid specifier, just print the character */
+                count += printf("%%%c", *format);
+            }
+        }
+        else
+        {
+            /* Regular character, print it */
+            putchar(*format);
+            count++;
+        }
 
-		format++;
-	}
+        format++; /* Move to the next character in the format string */
+    }
 
-	va_end(args);
-	return count;
+    va_end(args);
+
+    return count;
 }
 
 /**
- * main - Example usage of _printf.
- *
- * Return: Always 0.
+ * main - Entry point
+ * Return: 0 on success
  */
 int main(void)
 {
-	int result = _printf("Hello, %c! This is a %s example.%%\n", 'W', "printf");
-	printf("Number of characters printed: %d\n\n", result);
+    int result = _printf("This is a test: %c, %s, %%\n", 'A', "Hello, World!");
+    printf("Number of characters printed: %d\n", result);
 
-	return 0;
+    return 0;
 }
+
